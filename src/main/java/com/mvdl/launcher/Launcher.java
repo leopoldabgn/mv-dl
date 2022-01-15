f010c24b3dd6739d3ca933f87bde9dcde68765d3
Leopold Abignoli <leopold.abignoli@gmail.com>
2022-01-16 00:44:55 +0100

    Ajout d'un message d'erreur si l'utilisateur n'a pas installer: curl et yt-dlp

--- src/main/java/com/mvdl/launcher/Launcher.java
+++ src/main/java/com/mvdl/launcher/Launcher.java
@@ -3,11 +3,23 @@ package com.mvdl.launcher;
 import java.io.File;
 
 import com.mvdl.gui.GUI;
+import com.mvdl.model.Command;
 import com.mvdl.model.Preferences;
 
 public class Launcher {
 
     public static void main(String[] args) {
+        boolean ok = true;
+        if(!Command.checkPrgm("curl")) {
+            System.out.println("- You need to install curl   (ubuntu : sudo apt install curl)");
+            ok = false;
+        }
+        if(!Command.checkPrgm("yt-dlp")) {
+            System.out.println("- You need to install yt-dlp (ubuntu : sudo apt install yt-dlp)");
+            ok = false;
+        }
+        if(!ok)
+            return;
         // On recupere le fichier de prefs si il existe.
 		// Sinon, on en cree un nouveau
 		Preferences prefs = new Preferences(new File("music"));
