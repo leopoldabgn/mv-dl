42e8d5f9bb3797669b56318be7e58c2adccba4f0
Leopold Abignoli <leopold.abignoli@gmail.com>
2022-02-12 00:11:00 +0100

    Ajout de ffmpeg.exe pour convertir les music en mp3 proprement pour windows. Modification du pom.xml pour generer des jar avec les librairies incluses dedans. Ajout d'un test dans Launcher.java pour verifier si l'utilisateur linux a bien installer ffmpeg. Cependant plus besoin de verifier si yt-dlp est installer car le fichier binaire est deja dans le dossier.

--- src/main/java/com/mvdl/launcher/Launcher.java
+++ src/main/java/com/mvdl/launcher/Launcher.java
@@ -3,10 +3,14 @@ package com.mvdl.launcher;
 import java.io.File;
 
 import com.mvdl.gui.GUI;
+import com.mvdl.model.Command;
 import com.mvdl.model.Preferences;
 
 public class Launcher {
 
+    /**
+     * @param args
+     */
     public static void main(String[] args) {
         /* TEST pour telecharger une video
         Video video = new Video();
@@ -14,7 +18,7 @@ public class Launcher {
         Command.downloadVideo(new File("/home/leopold/Musique"), video, "22");
         */
 
-       // boolean ok = true;
+        boolean ok = true;
         // OLD METHOD
         // Peut servir au cas où la methode actuelle ne fonctionne plus.
         /*if(!Command.checkPrgm("curl")) {
@@ -30,8 +34,13 @@ public class Launcher {
             System.out.println("- You need to install yt-dlp (ubuntu : sudo apt install yt-dlp)");
             ok = false;
         }*/
-        // if(!ok)
-        //    return;
+        // On verifie ffmpeg pour linux
+        if(Command.isLinux() && !Command.checkPrgm("ffmpeg")) {
+            System.out.println("- You need to install ffmpeg (ubuntu : sudo apt install ffmpeg)");
+            ok = false;
+        }
+        if(!ok)
+           return;
         // On recupere le fichier de prefs si il existe.
 		// Sinon, on en cree un nouveau
 		Preferences prefs = new Preferences(new File("music"));
